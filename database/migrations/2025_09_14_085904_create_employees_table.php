fddfed177128c48fb45e580fe529344dccabe861
Mhdvk <muhammadmahdavikia78@gmail.com>
2025-09-25 21:15:23 +0700

    CRUD tabel employee

--- database/migrations/2025_09_14_085904_create_employees_table.php
+++ database/migrations/2025_09_14_085904_create_employees_table.php
@@ -12,15 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
-            $table->id(); // Primary Key
+            $table->id(); 
             $table->string('nama_lengkap', 100);
-            $table->string('email', 100)->unique(); // Email unik
+            $table->string('email', 100)->unique(); 
             $table->string('nomor_telepon', 15);
             $table->date('tanggal_lahir');
             $table->text('alamat');
             $table->date('tanggal_masuk');
             $table->enum('status', ['aktif', 'nonaktif'])->default('aktif');
-            $table->timestamps(); // created_at & updated_at
+            $table->timestamps(); 
         });
     }
 
