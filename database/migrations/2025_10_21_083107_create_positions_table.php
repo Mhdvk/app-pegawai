b721b23a4e7a238bd0be555b389f01abc8d4059a
Mhdvk <muhammadmahdavikia78@gmail.com>
2025-10-21 15:52:30 +0700

    Penambahan migrasi semua tabel

--- database/migrations/2025_10_21_083107_create_positions_table.php
+++ database/migrations/2025_10_21_083107_create_positions_table.php
@@ -6,15 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
             $table->string('nama_jabatan', 100);
             $table->decimal('gaji_pokok', 10, 2);
+            $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('positions');
